7d45f09641dfd6be252ee64908604f21ee563704
Parahat <parahat.iljanow@gmail.com>
2025-12-17 21:25:49 -0800

    cicd: scripts

--- backend/start-prod.sh
+++ backend/start-prod.sh
@@ -4,7 +4,7 @@ set -e
 echo "[backend:start-prod.sh] Starting production backend..."
 
 echo "[backend:start-prod.sh] Running migrations..."
-yarn db:migrate
+# yarn db:migrate
 
 echo "[backend:start-prod.sh] Migrations completed, starting server..."
 node dist/index.js
